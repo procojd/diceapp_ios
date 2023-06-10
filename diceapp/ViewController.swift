38bfa3923a7453cd1d5af5964eeb0be8a75f99d5
procojd <jatindayal2k19a614@gmail.com>
2023-06-10 13:02:41 +0530

    frist commit

--- diceapp/ViewController.swift
+++ diceapp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftBttn: UIImageView!
+    @IBOutlet weak var rightBttn: UIImageView!
+    var leftroll=1;
+    var rightroll=1;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollBttn(_ sender: UIButton) {
+        let randomInt = Int.random(in: 2..<8)
+        let randomInt2 = Int.random(in: 2..<8)
+        
+        leftBttn.image=UIImage(imageLiteralResourceName: "Layer \(randomInt)")
+        rightBttn.image=UIImage(imageLiteralResourceName: "Layer \(randomInt2)")
+    }
+    
 }
 
