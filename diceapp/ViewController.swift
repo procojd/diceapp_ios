40d8f46fd004f64af1cd46443f143fb119893e17
procojd <jatindayal2k19a614@gmail.com>
2023-06-10 09:07:18 +0530

    Initial Commit

---
+++ diceapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceapp
+//
+//  Created by Jatin Dayal on 10/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
